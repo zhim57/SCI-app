7014e4350c50c0bf1d651ccbe59d908af40cb77b
zhim57 <65269162+zhim57@users.noreply.github.com>
2023-05-07 13:30:55 -0400

    added encryption module

--- controllers/sciController1.js
+++ controllers/sciController1.js
@@ -1,20 +1,128 @@
 var request = require("request");
 const https = require("https");
 var express = require("express");
-const mongoose = require ("mongoose");
 const path = require("path");
 var router = express.Router();
 var axios = require("axios");
 var currentUser;
 var loggedUser;
-
+// import {currentUser } from "../public/assets/js/app.js";
 // var DL_API_KEY = process.env.DL_API_KEY;
 // console.log(DL_API_KEY);
+
 var pickup = require("../models/pickup"); // to fix
 var user = require("../models/user"); // to fix
 const { text } = require("body-parser");
 const { createBrotliCompress } = require("zlib");
 
+// Create all our routes and set up logic within those routes where required.
+
+router.get("/api/users/", function (req, res) {
+  //to fix
+  user.read1(req.query, function (data) {
+    res.send(data);
+  });
+});
+
+router.get("/home", function (req, res) {
+  res.render("home");
+});
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+router.get("/register", function (req, res) {
+  res.render("register");
+});
+
+router.post("/register1", function (req, res) {});
+
+//create a question
+router.post("/register", function (req, res) {
+  var cols = Object.entries(req.body).map((e) => e[0]);
+  var vals = Object.entries(req.body).map((e) => e[1]);
+
+  currentUser = {
+    u_email: req.body.u_email,
+
+    u_vessel: req.body.u_vessel,
+    u_last_name: req.body.u_last_name,
+    u_first_name: req.body.u_first_name,
+    u_cell: req.body.u_cell,
+    u_role: req.body.u_role,
+    u_whatsApp: req.body.u_whatsApp,
+  };
+
+  console.log(currentUser);
+  user
+    .create(cols, vals)
+    .then((results) => {
+      // res.json({ id: results.insertId });
+      res.render("seafarer", currentUser);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+loggedUser = {
+    u_email: req.body.username,
+
+  };
+
+
+
+  user.read4(
+    { table: "sci_users", cond: "u_email", u_email: username },
+    function (foundUser, err) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser[0].u_password === password) {
+
+loggedUser.id = foundUser[0].id;
+loggedUser.u_first_name = foundUser[0].u_first_name;
+loggedUser.u_last_name = foundUser[0].u_last_name;
+loggedUser.u_whatsApp = foundUser[0].u_whatsApp;
+loggedUser.u_cell = foundUser[0].u_cell;
+loggedUser.u_vessel = foundUser[0].u_vessel;
+loggedUser.u_role = foundUser[0].u_role;
+
+console.log("loggedUser");
+console.log(loggedUser);
+
+if(loggedUser.u_role === "seafarer"){
+
+  res.render("seafarer");
+}
+else if (loggedUser.u_role === "driver"){
+
+  res.render("driver");
+
+}
+
+else if (loggedUser.u_role === "dispatcher"){
+
+  res.render("dispatcher");
+
+}
+else if (loggedUser.u_role === "admin"){
+
+  res.render("dispatcher");
+
+}
+}
+          
+        } else {
+          console.log("no foundUser ?");
+        }
+      }
+    }
+  );
+});
 
 //create vessel
 router.post("/api/sci_pickups", function (req, res) {
--- server.js
+++ server.js
@@ -1,45 +1,195 @@
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
+var request = require("request");
+var mongoose = require("mongoose");
 var express = require("express");
 const path = require("path");
 const session = require("express-session");
+var router = express.Router();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const AES = require('mysql-aes');
+const AES = require("mysql-aes");
+const encrypt = require("mongoose-encryption");
 
 var PORT = process.env.PORT || 8082;
-dotenv.config({path: './.env'})
+dotenv.config({ path: "./.env" });
 
 var app = express();
+mongoose.connect("mongodb://localhost:27017/userDB");
 
-app.set('views', path.join(__dirname,"views"));
-app.set('view engine', 'ejs');
+const userSchema = {
+  u_email: String,
+  u_password: String,
+  u_vessel: String,
+  u_last_name: String,
+  u_first_name: String,
+  u_cell: String,
+  u_role: String,
+  u_whatsApp: String,
+  date: { type: Date, default: Date.now },
+  active: { type: Boolean, default: false },
+  outWard: { type: Boolean, default: false },
+  inWard: { type: Boolean, default: false }
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Import routes and give the server access to them.
-var routes = require("./controllers/sciController.js");
+// var routes = require("./controllers/sciController1.js");
+// var routes = require("./controllers/sciController.js");
 
-app.use(routes);
+// app.use(routes);
 
-app.use(session({
-  secret: "nodejs",
-  resave:true,
-  saveUninitialized:true
-}));
+app.use(
+  session({
+    secret: "nodejs",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-console.log(process.env.SECRET)
-console.log(process.env.API_KEY)
+console.log(process.env.SECRET);
+console.log(process.env.API_KEY);
+
+
+// Create all our routes and set up logic within those routes where required.
+
+app.get("/home", function (req, res) {
+  res.render("home");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+  // console.log(req.body);
+  const check = req.body.u_email;
+const newUser = new User ({
+    u_email: req.body.u_email,
+    u_password: req.body.password,
+    u_vessel: req.body.u_vessel,
+    u_last_name: req.body.u_last_name,
+    u_first_name: req.body.u_first_name,
+    u_cell: req.body.u_cell,
+    u_role: req.body.u_role,
+    u_whatsApp: req.body.u_whatsApp,
+  });
+
+  const foundPreviousUser = await User.find({u_email:check });
+console.log(foundPreviousUser);
+
+
+
+if(foundPreviousUser[0] ===undefined){
+
+  // console.log(newUser);
+  const result1  = await newUser.save();
+    console.log(result1);
+    res.render("seafarer");
+}
+
+else if (foundPreviousUser[0].u_email=== check){
+  data = {
+remarks: "Hello "+ foundPreviousUser[0].u_first_name+",  your email is already on file , please log in!" ,
+u_email:check,
+u_first_name: foundPreviousUser[0].u_first_name
+
+  }
+
+
+  res.render("login" , {data: data});
+}
+
+
+  
+
+
+
+});
+
+
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+loggedUser = {
+    u_email: req.body.username,
+
+  };
+
+
+
+  user.read4(
+    { table: "sci_users", cond: "u_email", u_email: username },
+    function (foundUser, err) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser[0].u_password === password) {
+
+loggedUser.id = foundUser[0].id;
+loggedUser.u_first_name = foundUser[0].u_first_name;
+loggedUser.u_last_name = foundUser[0].u_last_name;
+loggedUser.u_whatsApp = foundUser[0].u_whatsApp;
+loggedUser.u_cell = foundUser[0].u_cell;
+loggedUser.u_vessel = foundUser[0].u_vessel;
+loggedUser.u_role = foundUser[0].u_role;
+
+console.log("loggedUser");
+console.log(loggedUser);
+
+if(loggedUser.u_role === "seafarer"){
+
+  res.render("seafarer");
+}
+else if (loggedUser.u_role === "driver"){
+
+  res.render("driver");
+
+}
+
+else if (loggedUser.u_role === "dispatcher"){
+
+  res.render("dispatcher");
+
+}
+else if (loggedUser.u_role === "admin"){
+
+  res.render("dispatcher");
+
+}
+}
+          
+        } else {
+          console.log("no foundUser ?");
+        }
+      }
+    }
+  );
+});
+
+
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT+"/home");
+  console.log("Server listening on: http://localhost:" + PORT + "/home");
 });
